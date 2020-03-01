a0966e4b19b58f2447f8c7db959621e86324c1b2
ksethi1 <ksethi1@umbc.edu>
2020-03-01 03:01:55 -0500

    Used bcrypt to check the password with hash and implemented logic of login.

--- routes/api/users.js
+++ routes/api/users.js
@@ -23,7 +23,7 @@ router.post('/register', function (req, res) {
             return res.status(400).json({
                 email: 'Email already exists'
             });
-        }else {
+        } else {
             const avatar = gravatar.url(req.body.email, {
                 s: '200',//Size
                 r: 'pg',//Rating
@@ -35,16 +35,40 @@ router.post('/register', function (req, res) {
                 avatar: avatar,
                 password: req.body.password
             });
-            bcrypt.genSalt(10,(err,salt)=>{
-                bcrypt.hash(newUser.password,salt,(err,hash)=>{
-                    if(err) throw err;
-                    newUser.password=hash;
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                    if (err) throw err;
+                    newUser.password = hash;
                     newUser.save().then(user => res.json(user)).catch(err => console.log(err));
                 })
             })
         }
     })
 
+});
+
+//@route    GET api/users/login
+//@desc     Returning JWT token
+//@access   Public
+router.post('/login', function (req, res) {
+    const email = req.body.email;
+    const password = req.body.password;
+
+    //Find user by email
+    User.findOne({email}).then(user => {
+        if (!user) {
+            return res.status(404).json({email: 'User not found'});
+        }
+
+        //Check Password
+        bcrypt.compare(password, user.password).then(isMatch => {
+            if (isMatch) {
+                res.json({msg: 'Success'});
+            } else {
+                return res.status(400).json({password: 'Password incorrect'})
+            }
+        })
+    })
 });
 
 module.exports = router;
