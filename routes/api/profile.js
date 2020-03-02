f32c8475b2ed31a70b5c28cb871e3f1c69c77a36
ksethi1 <ksethi1@umbc.edu>
2020-03-01 21:44:21 -0500

    Created Profile model for DB. After login uses jwt for authorization. And ask for the profile related to that token. Here no profile is created yet so gives noprofile error.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
 
+//Load Profile model
+const Profile = require('../../models/Profile');
+//Load User profile
+const User = require('../../models/User');
+
+//@route    GET api/profile/test
+//@desc     Tests profile route
+//@access   Public
 router.get('/test', function (req, res) {
     res.json({
         key: 'profile test'
     });
 });
 
+//@route    GET api/profile
+//@desc     Get current user profile
+//@access   Private
+router.get('/', passport.authenticate('jwt', {session: false}), (req, res) => {
+    const errors = {};
+    //fetch current user profile
+    Profile.findOne({
+        user: req.user.id
+    }).then(profile => {
+        if (!profile) {
+            errors.noprofile = 'There is no profile for this user';
+            return res.status(404).json(errors);
+        }
+        res.json(profile);
+    })
+        .catch(err => res.status(404).json(err));
+});
 module.exports = router;
